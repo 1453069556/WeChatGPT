d3a86cf14f9b150b2037ff2397c8e9331b77f02e
MrTong <1453069556@qq.com>
2023-04-06 00:05:04 +0800

    去除测试

--- src/test/java/com/gpt/chatproject/ChatProjectApplicationTests.java
+++ src/test/java/com/gpt/chatproject/ChatProjectApplicationTests.java
@@ -1,11 +1,7 @@
 package com.gpt.chatproject;
 
 import com.gpt.chatproject.dao.FansDao;
-import com.gpt.chatproject.enums.DallResponseType;
-import com.gpt.chatproject.enums.DallSizeType;
-import com.gpt.chatproject.utils.DallUtils;
 import com.gpt.chatproject.utils.RedisUtils;
-import com.theokanning.openai.image.Image;
 import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.bean.menu.WxMenu;
 import me.chanjar.weixin.common.bean.menu.WxMenuButton;
@@ -14,15 +10,6 @@ import me.chanjar.weixin.mp.api.WxMpService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Base64;
-import java.util.List;
-import java.util.UUID;
 
 
 @SpringBootTest
@@ -67,41 +54,41 @@ class ChatProjectApplicationTests {
         this.wxService.getMenuService().menuCreate(menu);
     }
 
-    @Autowired
-    private DallUtils dallUtils;
-
-    @Test
-    public void testCreatImage() {
-//        String prompt = "the shot of a robotic dinosaur [description], intrincated, metal, high resolution, CGSociety, ZBrushCentral, digital illustration, detailed background, 3d shading";
-        String prompt = "An astronaut dressed in a space suit sits on a rock on the surface of the moon, holding a turkey in his hand and enjoying a Thanksgiving feast. The sky on the moon is dark with stars shining. Please use AI painting tools to create this image.";
-        List<Image> images = dallUtils.dall2(prompt, 1, DallSizeType.LARGE, DallResponseType.B64_JSON);
-        for (Image image : images) {
-            byte[] bytes = Base64.getDecoder().decode(image.getB64Json());
-            try {
-                File tempFile = File.createTempFile(UUID.randomUUID().toString(), ".jpg");
-                Files.write(tempFile.toPath(), bytes);
-                tempFile.deleteOnExit();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-    @Test
-    public void testCreatImage2() throws IOException {
-        // 读取JPG文件
-        BufferedImage imageLocal = ImageIO.read(new File("C:\\Users\\Ms Tong\\Desktop\\小c.jpg"));
-        // 写入PNG文件
-        ImageIO.write(imageLocal, "png", new File("C:\\Users\\Ms Tong\\Desktop\\小c.png"));
-        List<Image> images = dallUtils.dall2(new File("C:\\Users\\Ms Tong\\Desktop\\小c.png"), 1, DallSizeType.LARGE, DallResponseType.B64_JSON);
-        for (Image image : images) {
-            byte[] bytes = Base64.getDecoder().decode(image.getB64Json());
-            try {
-                File tempFile = File.createTempFile(UUID.randomUUID().toString(), ".jpg");
-                Files.write(tempFile.toPath(), bytes);
-                tempFile.deleteOnExit();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
+//    @Autowired
+//    private DallUtils dallUtils;
+//
+//    @Test
+//    public void testCreatImage() {
+////        String prompt = "the shot of a robotic dinosaur [description], intrincated, metal, high resolution, CGSociety, ZBrushCentral, digital illustration, detailed background, 3d shading";
+//        String prompt = "An astronaut dressed in a space suit sits on a rock on the surface of the moon, holding a turkey in his hand and enjoying a Thanksgiving feast. The sky on the moon is dark with stars shining. Please use AI painting tools to create this image.";
+//        List<Image> images = dallUtils.dall2(prompt, 1, DallSizeType.LARGE, DallResponseType.B64_JSON);
+//        for (Image image : images) {
+//            byte[] bytes = Base64.getDecoder().decode(image.getB64Json());
+//            try {
+//                File tempFile = File.createTempFile(UUID.randomUUID().toString(), ".jpg");
+//                Files.write(tempFile.toPath(), bytes);
+//                tempFile.deleteOnExit();
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//    }
+//    @Test
+//    public void testCreatImage2() throws IOException {
+//        // 读取JPG文件
+//        BufferedImage imageLocal = ImageIO.read(new File("C:\\Users\\Ms Tong\\Desktop\\小c.jpg"));
+//        // 写入PNG文件
+//        ImageIO.write(imageLocal, "png", new File("C:\\Users\\Ms Tong\\Desktop\\小c.png"));
+//        List<Image> images = dallUtils.dall2(new File("C:\\Users\\Ms Tong\\Desktop\\小c.png"), 1, DallSizeType.LARGE, DallResponseType.B64_JSON);
+//        for (Image image : images) {
+//            byte[] bytes = Base64.getDecoder().decode(image.getB64Json());
+//            try {
+//                File tempFile = File.createTempFile(UUID.randomUUID().toString(), ".jpg");
+//                Files.write(tempFile.toPath(), bytes);
+//                tempFile.deleteOnExit();
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//    }
 }
