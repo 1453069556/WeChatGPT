1b827ea9e467d9ca94821cdde275bdc6bf781197
MrTong <1453069556@qq.com>
2023-04-22 19:45:58 +0800

    支付功能上线，会员不限次数，支持更长内容

--- src/main/java/com/gpt/chatproject/utils/RedisUtils.java
+++ src/main/java/com/gpt/chatproject/utils/RedisUtils.java
@@ -3,6 +3,7 @@ package com.gpt.chatproject.utils;
 import com.gpt.chatproject.dao.MemberInfoDao;
 import com.gpt.chatproject.entity.MemberInfo;
 import com.gpt.chatproject.enums.ChatType;
+import com.gpt.chatproject.enums.MemberLevel;
 import com.gpt.chatproject.vo.MidjourneyRedisVo;
 import com.gpt.chatproject.vo.WxRedisCatchVo;
 import com.theokanning.openai.completion.chat.ChatMessage;
@@ -257,6 +258,20 @@ public class RedisUtils {
         return false;
     }
 
+    /**
+     * 设置会员状态
+     * @return
+     */
+    public void setMemberLevel(String fromUser, MemberLevel memberLevel) {
+        WxRedisCatchVo catchVo = (WxRedisCatchVo) redisTemplate.opsForValue().get(fromUser);
+        if (catchVo == null) {
+            WxRedisCatchVo wxRedisCatchVo = loadMember(new WxRedisCatchVo(CHAT_MAX_CATCH, ChatType.NORMAL), fromUser);
+            redisTemplate.opsForValue().set(fromUser, wxRedisCatchVo, CHAT_TIME_OUT, TimeUnit.SECONDS);
+            return;
+        }
+        catchVo.setMemberLevel(memberLevel.getType());
+        redisTemplate.opsForValue().set(fromUser, catchVo, CHAT_TIME_OUT, TimeUnit.SECONDS);
+    }
     /**
      * 获取聊天缓存
      *
