89152339839674fc2c9bfd6bef28e68cffd2fc58
MrTong <1453069556@qq.com>
2023-04-25 02:58:17 +0800

    会员策略修改,添加查看个人信息的按钮

--- src/main/java/com/gpt/chatproject/service/impl/MemberServiceImpl.java
+++ src/main/java/com/gpt/chatproject/service/impl/MemberServiceImpl.java
@@ -42,9 +42,9 @@ public class MemberServiceImpl implements MemberService {
         Date dateExpire = MyDateUtils.formatDate(memberInfoDaoByUserId.getExpireTime(), "yyyyMMddHHmmss");
         String dateExpireFormat = MyDateUtils.dateFormat(dateExpire, "yyyy-MM-dd HH:mm:ss");
         String dateStartFormat = MyDateUtils.dateFormat(dateStart, "yyyy-MM-dd HH:mm:ss");
-        memberInfo.append("会员开通日期: ").append(dateStartFormat).append("\n\n");
-        memberInfo.append("会员到期日期: ").append(dateExpireFormat).append("\n\n");
-        memberInfo.append("AI绘图剩余次数: ").append(imageNum).append("次");
+        memberInfo.append("会员开通日期:").append("\n").append(dateStartFormat).append("\n\n");
+        memberInfo.append("会员到期日期:").append("\n").append(dateExpireFormat).append("\n\n");
+        memberInfo.append("AI绘图剩余次数:").append(imageNum).append("次");
         return memberInfo.toString();
     }
 }
