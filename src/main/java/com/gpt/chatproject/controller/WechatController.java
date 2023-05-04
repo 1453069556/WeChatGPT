c47afe501504090a192eff0456388d8924b0d28a
MrTong <1453069556@qq.com>
2023-05-04 17:43:28 +0800

    更新正在输入..，开启DALL绘图

--- src/main/java/com/gpt/chatproject/controller/WechatController.java
+++ src/main/java/com/gpt/chatproject/controller/WechatController.java
@@ -84,8 +84,7 @@ public class WechatController {
                 .handler(weChatHandler.getWeChatImageReplyHandler()).end();
         WxMpXmlOutMessage outMessage = messageRouter.route(wxMpXmlMessage);
         redisUtils.resetCatchExpire(fromUser);
-        if (outMessage == null && WxConsts.XmlMsgType.TEXT.equals(wxMpXmlMessage.getMsgType())
-                && ChatType.NORMAL.equals(redisUtils.getCatch(fromUser).getChatType())) {
+        if (outMessage == null && WxConsts.XmlMsgType.TEXT.equals(wxMpXmlMessage.getMsgType())) {
             //为null，返回思考中
             return xmlMapper.writeValueAsString(
                     new WechatResponseTextMessage(wxMpXmlMessage.getFromUser(),
